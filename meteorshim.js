75009b883c4c227f27dd415a989af8179f4badc7
silentman11 <silent.r@wp.pl>
2017-09-30 21:19:22 +0200

    node change

--- meteorshim.js
+++ meteorshim.js
@@ -3,15 +3,38 @@ var fs = require('fs')
 
 // Setup env
 process.env.ROOT_URL = "http://" + process.env.OPENSHIFT_APP_DNS || "localhost";
-process.env.MONGO_URL = (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) || "mongodb://localhost:27017/meteor";
-process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
-process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+process.env.MONGO_URL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+if (process.env.MONGO_URL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = process.env.MONGO_URL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      process.env.MONGO_URL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    process.env.MONGO_URL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+
+
 
 // Show connection details on startup
 console.log("MONGO_URL IS: " + process.env.MONGO_URL);
 console.log("ROOT_URL IS: " + process.env.ROOT_URL);
 console.log("PORT: " + process.env.PORT);
 console.log("BIND_IP: " + process.env.BIND_IP);
+
 
 fs.stat('main.js', function(err, stat) {
 // if the meteor application bundle is missing, 
